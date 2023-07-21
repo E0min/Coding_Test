e6dac914d3ea17054611d3530e1dab0f1cba4034
E0min <bbok4yo@gmail.com>
2023-07-21 16:38:50 +0900

    cm

---
+++ Lv.1 연습문제/tempCodeRunnerFile.py
@@ -0,0 +1,16 @@
+def solution(food):
+    answer = ''
+    foodd=[]
+    for i in food:
+        if i%2==0:
+            foodd.append(i//2)
+        else:
+            foodd.append((i-1)//2)
+            
+    for a,b in zip(foodd[1:],range(1,len(foodd[1:]))):  
+        # a는 foodd 의 갯수, b는 foodd의 인덱스
+        for i in range(1,a+1):
+            answer = answer + b
+        print(answer)
+        
+    return answer
