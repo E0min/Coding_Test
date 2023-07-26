a5317409241bf4be9f5db407c9228a0342dd53b3
E0min <bbok4yo@gmail.com>
2023-07-27 02:13:38 +0900

    Update 팰린드롬 파티션.py

--- 재귀/팰린드롬 파티션.py
+++ 재귀/팰린드롬 파티션.py
@@ -1,12 +1,22 @@
-def palindrome(n): # n은 처음 팰린드롬을 구할 수 
+import sys
+def palindrome(n):
     if n<=1:
         count[0] += 1
-    else:
-        for i in range(n//2):
-            palindrome(i)
         
+    else:
+        for i in range(n//2+1):
+            if check[i] is not None:
+                palindrome(i)
+            else:
+                count[0] = count[0] + check[i]
 
-if __name__ =="__main__":
-    count =[1]
-    
+
+check = [] # 팰린드롬 파티션을 저장하는 배열
+count =[0] # 횟수를 저장하는 배열
+a = int(sys.stdin.readline())
+b = [int(sys.stdin.readline()) for _ in range(a)]
+for i in b:
+    palindrome(i)
     print(count[0])
+    count[0] = 0
+
