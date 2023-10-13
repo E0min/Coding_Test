66b0ad0976e5a021a411462d90f881d72e4184d8
E0min <bbok4yo@gmail.com>
2023-10-13 21:45:26 +0900

    cm

--- 그리디/P1931.java
+++ 그리디/P1931.java
@@ -1,17 +1,16 @@
 package 그리디;
 import java.util.Scanner;
-import java.util.Collections;
-import java.util.Arrays;
 
 public class P1931 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
-        int [][] meeting = new int[N][];
+        int [][] meeting = new int[N][2]; //0번째 열은 시작 시간, 1번째 열은 끝나는 시간
         int check = 0; // 회의실 마지막 사용 시간 체크
+        int fast = 0;
         int count = 0; // 회의의 갯수
-
+        
 
         for (int i=0 ; i<=N ; i++){ // 입력 받기 수행
             meeting[N][0] = sc.nextInt();
@@ -19,19 +18,19 @@ public class P1931 {
         }
         //회의실 배정
         // 1. check 시간 보다는 시작시간이 크고, 끝나는 시간이 가장 빠른 친구
-        
+        fast = meeting[0][1]; //끝나는 시간 암거나 배정
+        while(true){
         for (int j=0;j<=N;j++){ 
-            if(meeting[j][0]<check){continue;} // check 시간 보다 시작 시간이 이르다면 스킵한다.
-            else {
-                
+            if(meeting[j][0]<check){ // check 시간보다 시작 시간이 이르다면 스킵한다.
+                continue;
+            } 
+            else if(fast>meeting[j][1]) {  // 끝나는 시간이 가장 빠른 친구를 선택
+                fast = meeting[j][1]; // 끝나는 시간이 더 빠른 회의 시간을 fast에 할당
+                check = fast;
             }
-            //체크 시간보다 시작 시간이 늦는 친구들중 끝나는 시간이 가장 빠른 친구를 선택
-
+        
         }
-            
-
-
-
-
+        count++;
     }
+}
 }
